fa144fbef4552820c1cf0a100502bb216f8f3f9c
sara <sara@huhbub.com>
2016-12-05 17:04:52 +0000

    feature tests 'I can filter links by tag' - passes

--- app/app.rb
+++ app/app.rb
@@ -27,6 +27,7 @@ class BookmarkManager < Sinatra::Base
     redirect to('/links')
   end
 
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -16,7 +16,7 @@ feature 'Viewing links' do
   before(:each) do
     Link.create(url: 'http://www.huhbub.com', title: 'Huhbub', tags: [Tag.first_or_create(name: 'campaigns')])
     Link.create(url: 'http://www.popcultureplaypen.com', title: 'Pop Culture Playpen', tags: [Tag.first_or_create(name: 'pop culture')])
-    Link.create(url: 'http://www.theavclub.com', title: 'The A.V. Club', tags: [Tag.first_or_create(name: 'television')])
+    Link.create(url: 'http://www.theavclub.com', title: 'The AV Club', tags: [Tag.first_or_create(name: 'television')])
     Link.create(url: 'http://the-toast.net', title: 'The Toast', tags: [Tag.first_or_create(name: 'television')])
   end
 
@@ -25,8 +25,8 @@ feature 'Viewing links' do
 
   expect(page.status_code).to eq(200)
   within 'ul#links' do
-    expect(page).not_to_have_content('Huhbub')
-    expect(page).not_to_have_content('Pop Culture Playpen')
+    expect(page).not_to have_content('Huhbub')
+    expect(page).not_to have_content('Pop Culture Playpen')
     expect(page).to have_content('The AV Club')
     expect(page).to have_content ('The Toast')
     end
