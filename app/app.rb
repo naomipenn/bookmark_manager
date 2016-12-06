38641c3523accfd9df32c665c9f828509e8befba
sara <sara@huhbub.com>
2016-12-06 17:27:50 +0000

    improves sign up workflow

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,14 @@
 ENV["RACK_ENV"] ||="development"
 
   require 'sinatra/base'
+  require 'sinatra/flash'
   require './db/data_mapper_setup'
 
   class BookmarkManager < Sinatra::Base
     enable :sessions
     set :session_secret, 'super secret'
+
+    register Sinatra::Flash
 
     get '/' do
       'Hello Bookmarkmanager!'
@@ -37,16 +40,23 @@ ENV["RACK_ENV"] ||="development"
     end
 
     get '/users/new' do
+      @user = User.new
       erb :'users/new'
     end
 
     post '/users' do
-      user = User.create(email: params[:email],
+      @user = User.create(email: params[:email],
                   password: params[:password],
                   password_confirmation: params[:password_confirmation])
-      session[:user_id] = user.id
+      if @user.save
+      session[:user_id] = @user.id
       redirect to('/links')
+    else
+      flash.now[:notice] = 'Password and confirmation password do not match'
+      erb :'users/new'
     end
+  end
+
 
     helpers do
       def current_user
